4db558994b4899d2af70ee2cca8ec73752533f68
elhammoutidev <elhammoutidev@outlook.fr>
2026-01-21 00:24:09 +0100

    chore: sécurisation config BDD

--- config/bdd.exemple.php
+++ config/bdd.exemple.php
@@ -3,14 +3,13 @@
 function getPDO(): PDO
 {
     $hote = 'localhost';
-    $nomBase = 'sitetomtroc';      // mets ici le nom exact de ta base
+    $nomBase = 'tomtroc';
     $utilisateur = 'root';
-    $motDePasse = '';          // souvent vide sur XAMPP
+    $motDePasse = '';
 
     $dsn = "mysql:host=$hote;dbname=$nomBase;charset=utf8mb4";
 
     return new PDO($dsn, $utilisateur, $motDePasse, [
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
     ]);
 }
